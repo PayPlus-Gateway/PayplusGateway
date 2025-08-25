a128917064b204b8d02a322eb159d97d166b85a7
ShlomtzPP <shlomtzionPP>
2025-08-25 06:43:48 +0300

    Revert "fix for cart empty/via js / because it's not uS!"
    This reverts commit b19c84c726651b2f4e39ccce63e5697eb296e2f6.

--- view/frontend/web/js/view/payment/method-renderer/payplus_gateway.js
+++ view/frontend/web/js/view/payment/method-renderer/payplus_gateway.js
@@ -4,161 +4,165 @@
  */
 /*browser:true*/
 /*global define*/
-define([
-  "Magento_Checkout/js/model/payment/additional-validators",
-  "jquery",
-  "ko",
-  "mage/url",
-  "Magento_Vault/js/view/payment/vault-enabler",
-  "Magento_Checkout/js/view/payment/default",
-], function (additionalValidators, $, ko, url, VaultEnabler, Component) {
-  "use strict";
+define(
+    [
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'jquery',
+        'ko',
+        'mage/url',
+        'Magento_Vault/js/view/payment/vault-enabler',
+        'Magento_Checkout/js/view/payment/default',
+    ],
 
-  return Component.extend({
-    redirectAfterPlaceOrder: false,
-    getIframeURL: ko.observable(null),
-    iframeHeight: window.checkoutConfig.payment.payplus_gateway.iframe_height,
-    defaults: {
-      template: "Payplus_PayplusGateway/payment/form",
-      transactionResult: "",
-    },
+    function (
+        additionalValidators,
+        $,
+        ko,
+        url,
+        VaultEnabler,
+        Component
+    ) {
 
-    initialize: function () {
-      var self = this;
+        'use strict';
 
-      self._super();
-      this.vaultEnabler = new VaultEnabler();
-      this.vaultEnabler.setPaymentCode(this.getVaultCode());
-      return self;
-    },
+        return Component.extend({
+            redirectAfterPlaceOrder: false,
+            getIframeURL: ko.observable(null),
+            iframeHeight: window.checkoutConfig.payment.payplus_gateway.iframe_height,
+            defaults: {
+                template: 'Payplus_PayplusGateway/payment/form',
+                transactionResult: '',
+            },
 
-    initObservable: function () {
-      this._super().observe(["transactionResult"]);
-      return this;
-    },
 
-    /**
-     * @returns {Boolean}
-     */
-    isVaultEnabled: function () {
-      return this.vaultEnabler.isVaultEnabled();
-    },
+            initialize: function () {
+                var self = this;
 
-    /**
-     * Returns vault code.
-     *
-     * @returns {String}
-     */
-    getVaultCode: function () {
-      return window.checkoutConfig.payment[this.getCode()].ccVaultCode;
-    },
+                self._super();
+                this.vaultEnabler = new VaultEnabler();
+                this.vaultEnabler.setPaymentCode(this.getVaultCode());
+                return self;
+            },
 
-    bShowPayplusLogo: (method) => {
-      let bHidePayplusLogo =
-        window.checkoutConfig.payment[method].bHidePayplusLogo;
-      return bHidePayplusLogo;
-    },
-    getActive: function (method) {
-      let active = window.checkoutConfig.payment[method].active;
-      return active;
-    },
-    getTitle: function (method) {
-      let title = window.checkoutConfig.payment[method].title;
-      return title;
-    },
+            initObservable: function () {
 
-    getCode: function () {
-      return "payplus_gateway";
-    },
+                this._super()
+                    .observe([
+                        'transactionResult'
+                    ]);
+                return this;
+            },
 
-    getData: function () {
-      let payplusDataMethod = $(".payplus-select-method:checked").attr(
-        "payplus-data-method"
-      );
-      var data = {
-        method: this.item.method,
-        additional_data: {
-          transaction_result: this.transactionResult(),
-          payplusmethodreq: payplusDataMethod,
-        },
-      };
+            /**
+             * @returns {Boolean}
+             */
+            isVaultEnabled: function () {
+                return this.vaultEnabler.isVaultEnabled();
+            },
 
-      data["additional_data"] = _.extend(
-        data["additional_data"],
-        this.additionalData
-      );
-      this.vaultEnabler.visitAdditionalData(data);
 
-      return data;
-    },
+            /**
+             * Returns vault code.
+             *
+             * @returns {String}
+             */
+            getVaultCode: function () {
+                return window.checkoutConfig.payment[this.getCode()].ccVaultCode;
+            },
 
-    placeOrder: function (data, event) {
-      var self = this;
-      if (event) {
-        event.preventDefault();
-      }
+            bShowPayplusLogo: (method) => {
+                let bHidePayplusLogo =window.checkoutConfig.payment[method].bHidePayplusLogo
+                return bHidePayplusLogo;
+            },
+            getActive:function (method){
 
-      if (
-        this.validate() &&
-        additionalValidators.validate() &&
-        this.isPlaceOrderActionAllowed() === true
-      ) {
-        this.isPlaceOrderActionAllowed(false);
+                let active =window.checkoutConfig.payment[method].active
+                return active;
+            },
+            getTitle:function (method){
+                let title =window.checkoutConfig.payment[method].title
+                return title;
+            },
 
-        this.getPlaceOrderDeferredObject()
-          .done(function (response) {
-            self.afterPlaceOrder(response);
+            getCode: function () {
+                return 'payplus_gateway';
+            },
 
-            if (self.redirectAfterPlaceOrder) {
-              redirectOnSuccessAction.execute();
-            }
-          })
-          .always(function () {
-            self.isPlaceOrderActionAllowed(true);
-          });
+            getData: function () {
+                let payplusDataMethod = $('.payplus-select-method:checked').attr("payplus-data-method");
+                var data = {
+                    'method': this.item.method,
+                    'additional_data': {
+                        'transaction_result': this.transactionResult(),
+                        'payplusmethodreq': payplusDataMethod,
+                    }
+                };
 
-        return true;
-      }
+                data['additional_data'] = _.extend(data['additional_data'], this.additionalData);
+                this.vaultEnabler.visitAdditionalData(data);
 
-      return false;
-    },
+                return data;
+            },
 
-    afterPlaceOrder: function (Response) {
-      if (isNaN(Response)) {
-        alert("Error processing your request");
-        return false;
-      }
+            placeOrder: function (data, event) {
+                var self = this;
+                if (event) {
+                    event.preventDefault();
+                }
 
-      $.ajax({
-        url: url.build(`/payplus_gateway/ws/getredirect/orderid/${Response}`),
-        dataType: "json",
-        type: "get",
-        success: (Response) => {
-          // Reload cart data AFTER GetRedirect has restored the quote
-          require(["Magento_Customer/js/customer-data"], function (
-            customerData
-          ) {
-            customerData.reload(["cart"], false);
+                if (this.validate() &&
+                    additionalValidators.validate() &&
+                    this.isPlaceOrderActionAllowed() === true
+                ) {
+                    this.isPlaceOrderActionAllowed(false);
 
-            // Also invalidate and reload to force fresh data
-            customerData.invalidate(["cart"]);
-            customerData.reload(["cart"], false);
-          });
+                    this.getPlaceOrderDeferredObject()
+                        .done(
+                            function (response) {
+                                self.afterPlaceOrder(response);
 
-          if (Response.status == "success" && Response.redirectUrl) {
-            if (
-              window.checkoutConfig.payment.payplus_gateway.form_type ==
-              "iframe"
-            ) {
-              this.getIframeURL(Response.redirectUrl);
-            } else {
-              window.location.replace(Response.redirectUrl);
-            }
-          } else {
-            alert("Error."); // for now
-          }
-        },
-      });
-    },
-  });
-});
+                                if (self.redirectAfterPlaceOrder) {
+                                    redirectOnSuccessAction.execute();
+                                }
+                            }
+                        ).always(
+                            function () {
+                                self.isPlaceOrderActionAllowed(true);
+                            }
+                        );
+
+                    return true;
+                }
+
+                return false;
+            },
+
+
+            afterPlaceOrder: function (Response) {
+
+                if (isNaN(Response)) {
+                    alert('Error processing your request');
+                    return false;
+                }
+                $.ajax({
+                    url: url.build(`/payplus_gateway/ws/getredirect/orderid/${Response}`),
+                    dataType: 'json',
+                    type: 'get',
+                    success: (Response) => {
+                        if (Response.status == 'success' && Response.redirectUrl) {
+                            if (window.checkoutConfig.payment.payplus_gateway.form_type == 'iframe') {
+                                this.getIframeURL(Response.redirectUrl);
+                            } else {
+                                window.location.replace(Response.redirectUrl);
+                            }
+                        } else {
+                            alert("Error."); // for now
+                        }
+                    }
+                });
+            },
+        });
+
+
+    }
+);
