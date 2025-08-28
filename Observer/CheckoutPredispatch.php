e20ce2a67fd656a4c7746d1d84bab26187a519bf
Shlomtzion PayPlus Mac <shlomtion@gmail.com>
2025-08-28 19:45:05 +0300

    feature/coupon un-redeem/order cancel if...

---
+++ Observer/CheckoutPredispatch.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Payplus\PayplusGateway\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Magento\Sales\Model\Order;
+use Magento\SalesRule\Model\Coupon;
+use Magento\SalesRule\Model\ResourceModel\Coupon\CollectionFactory as CouponCollectionFactory;
+use Psr\Log\LoggerInterface;
+
+class CheckoutPredispatch implements ObserverInterface
+{
+    /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+
+    /**
+     * @var OrderCollectionFactory
+     */
+    protected $orderCollectionFactory;
+
+    /**
+     * @var CouponCollectionFactory
+     */
+    protected $couponCollectionFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    protected $dateTime;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\TimezoneInterface
+     */
+    protected $timezone;
+
+    public function __construct(
+        CustomerSession $customerSession,
+        OrderCollectionFactory $orderCollectionFactory,
+        CouponCollectionFactory $couponCollectionFactory,
+        LoggerInterface $logger,
+        \Magento\Framework\Stdlib\DateTime\DateTime $dateTime,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone
+    ) {
+        $this->customerSession = $customerSession;
+        $this->orderCollectionFactory = $orderCollectionFactory;
+        $this->couponCollectionFactory = $couponCollectionFactory;
+        $this->logger = $logger;
+        $this->dateTime = $dateTime;
+        $this->timezone = $timezone;
+    }
+
+    /**
+     * Execute observer
+     *
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        try {
+            // Only process for logged-in customers
+            if (!$this->customerSession->isLoggedIn()) {
+                return;
+            }
+
+            $customerId = $this->customerSession->getCustomerId();
+            $this->processPendingOrdersWithCoupons($customerId);
+        } catch (\Exception $e) {
+            $this->logger->error('PayPlus Gateway - Error in CheckoutPredispatch observer: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Process pending orders with coupons for the current customer
+     *
+     * @param int $customerId
+     * @return void
+     */
+    protected function processPendingOrdersWithCoupons($customerId)
+    {
+        // Get today's date range in store timezone
+        $todayStart = $this->timezone->date()->setTime(0, 0, 0)->format('Y-m-d H:i:s');
+        $todayEnd = $this->timezone->date()->setTime(23, 59, 59)->format('Y-m-d H:i:s');
+
+        // Find pending orders from today for this customer
+        $orderCollection = $this->orderCollectionFactory->create();
+        $orderCollection->addFieldToFilter('customer_id', $customerId)
+            ->addFieldToFilter('state', Order::STATE_PENDING_PAYMENT)
+            ->addFieldToFilter('status', 'pending_payment')
+            ->addFieldToFilter('created_at', ['gteq' => $todayStart])
+            ->addFieldToFilter('created_at', ['lteq' => $todayEnd])
+            ->addFieldToFilter('coupon_code', ['notnull' => true])
+            ->addFieldToFilter('coupon_code', ['neq' => '']);
+
+        foreach ($orderCollection as $order) {
+            $couponCode = $order->getCouponCode();
+            if ($couponCode) {
+                $this->restoreCouponUsage($couponCode, $order);
+                $this->cancelOrder($order);
+            }
+        }
+    }
+
+    /**
+     * Restore coupon usage by decrementing the times_used counter
+     *
+     * @param string $couponCode
+     * @param Order $order
+     * @return void
+     */
+    protected function restoreCouponUsage($couponCode, $order)
+    {
+        try {
+            $couponCollection = $this->couponCollectionFactory->create();
+            $couponCollection->addFieldToFilter('code', $couponCode);
+
+            $coupon = $couponCollection->getFirstItem();
+            if ($coupon && $coupon->getId()) {
+                $currentUsage = $coupon->getTimesUsed();
+                if ($currentUsage > 0) {
+                    $coupon->setTimesUsed($currentUsage - 1);
+                    $coupon->save();
+
+                    $this->logger->info(
+                        'PayPlus Gateway - Restored coupon usage',
+                        [
+                            'coupon_code' => $couponCode,
+                            'order_id' => $order->getId(),
+                            'previous_usage' => $currentUsage,
+                            'new_usage' => $currentUsage - 1
+                        ]
+                    );
+                }
+            }
+        } catch (\Exception $e) {
+            $this->logger->error(
+                'PayPlus Gateway - Error restoring coupon usage: ' . $e->getMessage(),
+                [
+                    'coupon_code' => $couponCode,
+                    'order_id' => $order->getId()
+                ]
+            );
+        }
+    }
+
+    /**
+     * Cancel the order
+     *
+     * @param Order $order
+     * @return void
+     */
+    protected function cancelOrder($order)
+    {
+        try {
+            if ($order->canCancel()) {
+                $order->cancel();
+                $order->addCommentToStatusHistory(
+                    'Order cancelled automatically due to new checkout session with same coupon.',
+                    false,
+                    false
+                );
+                $order->save();
+
+                $this->logger->info(
+                    'PayPlus Gateway - Cancelled pending order with coupon',
+                    [
+                        'order_id' => $order->getId(),
+                        'coupon_code' => $order->getCouponCode(),
+                        'customer_id' => $order->getCustomerId()
+                    ]
+                );
+            }
+        } catch (\Exception $e) {
+            $this->logger->error(
+                'PayPlus Gateway - Error cancelling order: ' . $e->getMessage(),
+                [
+                    'order_id' => $order->getId()
+                ]
+            );
+        }
+    }
+}
