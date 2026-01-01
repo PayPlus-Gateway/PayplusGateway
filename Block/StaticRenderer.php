dc3a37bb2979e2ae36f3ac3faaaec0115a1a34c8
ShlomtzPP <shlomtzionPP>
2026-01-01 11:00:26 +0200

    tweak/Show apple pay script loading in settings always and activate it no matter which page display method is selected as long as it is set to yes.

--- Block/StaticRenderer.php
+++ Block/StaticRenderer.php
@@ -6,11 +6,14 @@ class StaticRenderer extends \Magento\Backend\Block\AbstractBlock
     protected function _construct()
     {
         $scp = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        if ($this
-            ->_scopeConfig
-            ->getValue('payment/payplus_gateway/display_settings/iframe_or_redirect', $scp) == 'iframe'
-            && $this->_scopeConfig->getValue('payment/payplus_gateway/display_settings/import_applepay_script', $scp)
-        ) {
+        
+        // Check if Apple Pay script is enabled (works for both iframe and redirect modes)
+        $isApplePayScriptEnabled = $this->_scopeConfig->isSetFlag(
+            'payment/payplus_gateway/display_settings/import_applepay_script',
+            $scp
+        );
+        
+        if ($isApplePayScriptEnabled) {
             $api_test_mode = $this
                 ->_scopeConfig
                 ->getValue('payment/payplus_gateway/api_configuration/dev_mode', $scp) == 1;
