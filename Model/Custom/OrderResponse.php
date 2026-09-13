37343766235ea36236ac455ad3719d28daab0b14
ShlomtzPP <shlomo@payplus.co.il>
2026-09-14 00:27:25 +0300

    Revert "tweak/processing is default for orders syncer not complete/"
    This reverts commit 2796eceebe785e8aaf2e053787a1db6259c2b127.

--- Model/Custom/OrderResponse.php
+++ Model/Custom/OrderResponse.php
@@ -29,7 +29,7 @@ class OrderResponse
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
 
-        $this->statusGlobal = ($this->statusGlobal) ? $this->statusGlobal : 'processing';
+        $this->statusGlobal = ($this->statusGlobal) ? $this->statusGlobal : 'complete';
         $this->stateOGlobal = $this->config->getValue(
             'payment/payplus_gateway/api_configuration/state_order_payplus',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
@@ -48,7 +48,7 @@ class OrderResponse
             'payment/payplus_gateway/api_configuration/status_approval_order_payplus',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         ) : 'holded';
-        $this->stateOGlobal = ($this->stateOGlobal) ?  $this->stateOGlobal : 'processing';
+        $this->stateOGlobal = ($this->stateOGlobal) ?  $this->stateOGlobal : 'complete';
     }
     public function processResponse($params, $direct = false)
     {
